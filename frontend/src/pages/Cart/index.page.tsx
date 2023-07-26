4d11e231abbe34605b0b92f89d7d000af41273ec
Harkirat Singh Virdi <57691094+HarkiratVirdi@users.noreply.github.com>
2023-07-25 21:54:35 -0400

    cart functionality in making

--- frontend/src/pages/Cart/index.page.tsx
+++ frontend/src/pages/Cart/index.page.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import Product from '../../components/Product/index.comp';
 import Cart from '../../components/Cart/index.comp';
-
 import HeaderSearch from '../../components/Header/index.comp';
-import BannerCarousel from '../../components/BannerCarousel/index.comp';
-
-
 
 const CartPage = () => {
-    return (
-      <>
+  return (
+    <>
       <HeaderSearch />
+      <div className="mt-12 mx-4">
+        <Cart />
+      </div>
+    </>
+  );
+};
 
-        <div className="mt-12 mx-4">
-          <Cart />
-        </div>
-      </>
-    );
-  };
-
-  export default CartPage;
+export default CartPage;
